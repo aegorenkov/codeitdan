1c46f87f6fdc245c202d0133277b3acaf3bab866
Alex Egorenkov <aegorenk@gmail.com>
2018-03-28 19:57:15 -0400

    Init

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,4 +1,19 @@
 import "../css/popup.css";
 import hello from "./popup/example";
 
-hello();
+// var newURL = "about:blank";
+// chrome.tabs.create({ url: newURL });
+
+// chrome.tabs.create({
+//   url: "about:blank"
+// }, function (tab) {
+//   alert(tab);
+// });
+
+// chrome.tabs.create({url: "about:blank"}).then(() => {
+//   chrome.tabs.executeScript({
+//     code: `console.log('location:', window.location.href);`
+//   });
+// });
+
+chrome.tabs.create({'url': chrome.extension.getURL('background.html')});
